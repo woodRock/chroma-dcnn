8ca7ced30b32da2b9532dfb7667bac55a8d25072
woodRock <j.r.h.wood98@gmail.com>
2026-05-28 16:09:56 +1200

    Remove reference to old datasets

--- src/msformer/downstream/__init__.py
+++ src/msformer/downstream/__init__.py
@@ -1,4 +1,3 @@
-from msformer.downstream.olive_oil import load_olive_oil_data
-from msformer.downstream.hemp_seed import load_hemp_seed_data
+from msformer.downstream.fish_oil import load_fish_oil_data
 
-__all__ = ["load_olive_oil_data", "load_hemp_seed_data"]
+__all__ = ["load_fish_oil_data"]
